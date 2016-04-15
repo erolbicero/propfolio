d6e5ca8a510840655cbcbf330a05e9d5e6a6be3a
erolbicero <erol.biceroglu@alumni.utoronto.ca>
2016-04-15 18:51:06 -0400

    Updated plots to not require packages to be loaded, as well as added ability to toggle plotly output for interactive graphs

--- R/88-Plot-E-plotRiskReturnScatter.R
+++ R/88-Plot-E-plotRiskReturnScatter.R
@@ -5,7 +5,7 @@
 #' @param yTitle character string of y-axis title; defaults to "Annualized Return"
 #' @param xTitle title for x-axis, defaults to "Annualized Risk"
 #' @param scale is a character string, one of "daily", "weekly", "monthly", "quarterly", "yearly"
-#' 
+#' @param interactive logical, whether or not to render in plotly for interactive use of plot; defaults to FALSE
 #'
 #' @return a timeseries plot (ggplot)
 #'
@@ -13,7 +13,7 @@
 #' none
 #'
 #' @export
-plotRiskReturnScatter <- function(xtsTimeSeriesObject, plotTitle, yTitle = "Annualized Return", xTitle = "Annualized Risk", scale = "daily"){
+plotRiskReturnScatter <- function(xtsTimeSeriesObject, plotTitle, yTitle = "Annualized Return", xTitle = "Annualized Risk", scale = "daily", interactive = FALSE){
   
   scalingFactor <- 
     switch(scale
@@ -38,30 +38,36 @@ plotRiskReturnScatter <- function(xtsTimeSeriesObject, plotTitle, yTitle = "Annu
   
   xLimHigh <- max(scatterData$Risk)*1.5
   
-  ggplot2::ggplot( data = scatterData, aes(x = Risk, y = Return)) +
-    geom_point(aes(colour = Names), size = 1) +
-    scale_colour_brewer(palette = ifelse(ncol(xtsTimeSeriesObject)<=9,"Set1","Paired"), type = "qual") + 
-    labs(ylab(yTitle)) + 
-    labs(xlab(xTitle)) + 
-    theme_bw() +
-    facet_grid(~facetTitle) + 
-    theme(panel.grid.major = element_line(colour = "grey")
-          , panel.grid.minor = element_line(colour = "grey", linetype = "dotted")
-          , panel.border = element_rect(colour = "black")
-          , strip.background = element_rect(fill = "dark blue", colour = "black", size = 1)
-          , strip.text.x = element_text(colour = "white", size = 17, face = "bold")
-          , axis.title = element_text(size = 15)
-          , axis.text = element_text(size = 13)
+  plotObject <- 
+  ggplot2::ggplot( data = scatterData, ggplot2::aes(x = Risk, y = Return)) +
+    ggplot2::geom_point(ggplot2::aes(colour = Names), size = 1) +
+    ggplot2::scale_colour_brewer(palette = ifelse(ncol(xtsTimeSeriesObject)<=9,"Set1","Paired"), type = "qual") + 
+    ggplot2::labs(ggplot2::ylab(yTitle)) + 
+    ggplot2::labs(ggplot2::xlab(xTitle)) + 
+    ggplot2::theme_bw() +
+    ggplot2::facet_grid(~facetTitle) + 
+    ggplot2::theme(panel.grid.major = ggplot2::element_line(colour = "grey")
+          , panel.grid.minor = ggplot2::element_line(colour = "grey", linetype = "dotted")
+          , panel.border = ggplot2::element_rect(colour = "black")
+          , strip.background = ggplot2::element_rect(fill = "dark blue", colour = "black", size = 1)
+          , strip.text.x = ggplot2::element_text(colour = "white", size = 17, face = "bold")
+          , axis.title = ggplot2::element_text(size = 15)
+          , axis.text = ggplot2::element_text(size = 13)
           , legend.position = "none"
     ) +
-    scale_y_continuous(labels = scales::percent) + 
-    scale_x_continuous(labels = scales::percent) +
-    ggrepel::geom_text_repel(label = colnames(xtsTimeSeriesObject), segment.size = 0, aes(colour = Names)) + 
-    geom_hline(aes(yintercept=0)) + 
-    geom_vline(aes(xintercept=0)) + 
-    geom_abline(intercept = 0, slope = 3, linetype = "dashed", colour = "grey") + 
-    geom_abline(intercept = 0, slope = 2, linetype = "dashed", colour = "grey") + 
-    geom_abline(intercept = 0, slope = 1, linetype = "dashed", colour = "grey")
+    ggplot2::scale_y_continuous(labels = scales::percent) + 
+    ggplot2::scale_x_continuous(labels = scales::percent) +
+    ggrepel::geom_text_repel(label = colnames(xtsTimeSeriesObject), segment.size = 0, ggplot2::aes(colour = Names)) + 
+    ggplot2::geom_hline(ggplot2::aes(yintercept=0)) + 
+    ggplot2::geom_vline(ggplot2::aes(xintercept=0)) + 
+    ggplot2::geom_abline(intercept = 0, slope = 3, linetype = "dashed", colour = "grey") + 
+    ggplot2::geom_abline(intercept = 0, slope = 2, linetype = "dashed", colour = "grey") + 
+    ggplot2::geom_abline(intercept = 0, slope = 1, linetype = "dashed", colour = "grey")
   
+  if(interactive){
+    plotly::ggplotly(plotObject)
+  }else{
+    plotObject
+  }
   
 }
--- R/88-Plot-E-plotTimeSeriesObject.R
+++ R/88-Plot-E-plotTimeSeriesObject.R
@@ -3,10 +3,10 @@
 #' @param xtsTimeSeriesObject an xts matrix of *levels*
 #' @param plotTitle character string of plot title
 #' @param yTitle character string of y-axis title
-#' @param yLabelType is an object from the *scales* package, one of dollar, percent, scientific, comma; defaults to dollar
+#' @param yLabelType is a string, one of dollar, percent, scientific, comma; defaults to dollar
 #' @param xTitle title for x-axis, defaults to "Date"
 #' @param levels logical, if FALSE (default) indicates that the object contents are returns, if TRUE, indicates that the object contents are levels (e.g. Prices)
-#' 
+#' @param interactive logical, whether or not to render in plotly for interactive use of plot; defaults to FALSE
 #'
 #' @return a timeseries plot (ggplot)
 #'
@@ -14,28 +14,44 @@
 #' none
 #'
 #' @export
-plotTimeSeriesObject <- function(xtsTimeSeriesObject, plotTitle, yTitle, yLabelType = scales::dollar, xTitle = "Date", levels = FALSE){
+plotTimeSeriesObject <- function(xtsTimeSeriesObject, plotTitle, yTitle, yLabelType = "dollar", xTitle = "Date", levels = FALSE, interactive = FALSE){
 
   if(levels){
     xtsTimeSeriesObject <- cumprod(xtsTimeSeriesObject + 1) #convert to levels
   }
   
+  yLabelTypeValue <- switch (yLabelType
+                        , "dollar" = scales::dollar
+                        , "percent" = scales::percent
+                        , "scientific" = scales::scientific
+                        , "comma" = scales::comma
+    
+  )
+  
   plotData <- cbind( reshape2::melt(data = toDF(xtsTimeSeriesObject), id.vars = 1), Title = plotTitle)
   colnames(plotData) <- c(xTitle, "Ticker", "value","facetTitle")
   
-  ggplot2::ggplot( data = plotData, aes(x = Date, y = value)) +
-  geom_line(aes(colour = Ticker), size = 1) +
-  scale_colour_brewer(palette = ifelse(ncol(xtsTimeSeriesObject)<=9,"Set1","Paired"), type = "qual") + 
-  labs(ylab(yTitle)) + 
-  theme_bw() +
-  facet_grid(~facetTitle) + 
-  theme(panel.grid.major = element_line(colour = "grey")
-        , panel.grid.minor = element_line(colour = "grey", linetype = "dotted")
-        , panel.border = element_rect(colour = "black")
-        , strip.background = element_rect(fill = "dark blue", colour = "black", size = 1)
-        , strip.text.x = element_text(colour = "white", size = 17, face = "bold")
-        , axis.title = element_text(size = 15)
-        , axis.text = element_text(size = 13)
+  plotObject <-
+  ggplot2::ggplot( data = plotData, ggplot2::aes(x = Date, y = value)) +
+  ggplot2::geom_line(ggplot2::aes(colour = Ticker), size = 1) +
+  ggplot2::scale_colour_brewer(palette = ifelse(ncol(xtsTimeSeriesObject)<=9,"Set1","Paired"), type = "qual") + 
+  ggplot2::labs(ggplot2::ylab(yTitle)) + 
+  ggplot2::theme_bw() +
+  ggplot2::facet_grid(~facetTitle) + 
+  ggplot2::theme(panel.grid.major = ggplot2::element_line(colour = "grey")
+        , panel.grid.minor = ggplot2::element_line(colour = "grey", linetype = "dotted")
+        , panel.border = ggplot2::element_rect(colour = "black")
+        , strip.background = ggplot2::element_rect(fill = "dark blue", colour = "black", size = 1)
+        , strip.text.x = ggplot2::element_text(colour = "white", size = 17, face = "bold")
+        , axis.title = ggplot2::element_text(size = 15)
+        , axis.text = ggplot2::element_text(size = 13)
   ) +
-  scale_y_continuous(labels = yLabelType)
+  ggplot2::scale_y_continuous(labels = yLabelTypeValue)
+  
+  if(interactive){
+      plotly::ggplotly(plotObject)
+  }else{
+    plotObject
+  }
+  
 }
