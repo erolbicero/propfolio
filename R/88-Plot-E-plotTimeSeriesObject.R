a48e1c73fd5de78a6e7e282ad86c2d87090aa347
erolbicero <erol.biceroglu@alumni.utoronto.ca>
2016-06-06 19:16:10 -0400

    enhanced functionality

--- R/88-Plot-E-plotTimeSeriesObject.R
+++ R/88-Plot-E-plotTimeSeriesObject.R
@@ -3,9 +3,10 @@
 #' @param xtsTimeSeriesObject an xts matrix of *levels*
 #' @param plotTitle character string of plot title
 #' @param yTitle character string of y-axis title
-#' @param yLabelType is a string, one of dollar, percent, scientific, comma; defaults to dollar
+#' @param yLabelType is a string, one of "dollar", "percent", "scientific", "comma"; defaults to comma
 #' @param xTitle title for x-axis, defaults to "Date"
-#' @param levels logical, if FALSE (default) indicates that the object contents are returns, if TRUE, indicates that the object contents are levels (e.g. Prices)
+#' @param legendTitle title for legend, defaults to "Ticker"
+#' @param levels character string, one of "linear", "continuous" or "raw"; if "raw", passes the timeSeries as-is for plotting, othwerise, will convert vector into an index (e.g. Prices) based on the selection of "linear" or "continuous" returns; defaults to "raw"
 #' @param interactive logical, whether or not to render in plotly for interactive use of plot; defaults to FALSE
 #'
 #' @return a timeseries plot (ggplot)
@@ -14,11 +15,17 @@
 #' none
 #'
 #' @export
-plotTimeSeriesObject <- function(xtsTimeSeriesObject, plotTitle, yTitle, yLabelType = "dollar", xTitle = "Date", levels = FALSE, interactive = FALSE){
+plotTimeSeriesObject <- function(xtsTimeSeriesObject, plotTitle = "Plot of Time Series", yTitle = "Time Series Value", yLabelType = "comma", xTitle = "Date", legendTitle = "Ticker", levels = "raw", interactive = FALSE){
 
-  if(levels){
-    xtsTimeSeriesObject <- cumprod(xtsTimeSeriesObject + 1) #convert to levels
-  }
+  # if(levels){
+  #   xtsTimeSeriesObject <- cumprod(xtsTimeSeriesObject + 1) #convert to levels
+  # }
+  
+  xtsTimeSeriesObject <- switch(levels
+    , linear = cumprod(xtsTimeSeriesObject + 1) #convert to levels
+    , continuous = cumprod(exp(xtsTimeSeriesObject)) #convert to levels
+    , raw = xtsTimeSeriesObject
+  )
   
   yLabelTypeValue <- switch (yLabelType
                         , "dollar" = scales::dollar
@@ -29,13 +36,14 @@ plotTimeSeriesObject <- function(xtsTimeSeriesObject, plotTitle, yTitle, yLabelT
   )
   
   plotData <- cbind( reshape2::melt(data = toDF(xtsTimeSeriesObject), id.vars = 1), Title = plotTitle)
-  colnames(plotData) <- c(xTitle, "Ticker", "value","facetTitle")
+  colnames(plotData) <- c(xTitle, legendTitle, "value","facetTitle")
   
   plotObject <-
-  ggplot2::ggplot( data = plotData, ggplot2::aes(x = Date, y = value)) +
-  ggplot2::geom_line(ggplot2::aes(colour = Ticker), size = 1) +
-  ggplot2::scale_colour_brewer(palette = ifelse(ncol(xtsTimeSeriesObject)<=9,"Set1","Paired"), type = "qual") + 
+  ggplot2::ggplot( data = plotData, ggplot2::aes(x = eval(parse(text = xTitle)), y = value)) +
+  ggplot2::geom_line(ggplot2::aes(colour = eval(parse(text = legendTitle))), size = 1) +
+  ggplot2::scale_colour_brewer(name = legendTitle, palette = ifelse(ncol(xtsTimeSeriesObject)<=9,"Set1","Paired"), type = "qual") + 
   ggplot2::labs(ggplot2::ylab(yTitle)) + 
+  ggplot2::labs(ggplot2::xlab(xTitle)) + 
   ggplot2::theme_bw() +
   ggplot2::facet_grid(~facetTitle) + 
   ggplot2::theme(panel.grid.major = ggplot2::element_line(colour = "grey")
