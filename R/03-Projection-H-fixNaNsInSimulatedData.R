4d9b72eeb72675a8a6d8e66e2e7e614c6f3a452f
erolbicero <erol.biceroglu@alumni.utoronto.ca>
2016-06-12 14:02:28 -0400

    added case for tails

--- R/03-Projection-H-fixNaNsInSimulatedData.R
+++ R/03-Projection-H-fixNaNsInSimulatedData.R
@@ -14,8 +14,48 @@ function(copulaVector, simulatedDataVector, numSimulations, percentDataToRemove 
   firstNaN <- min(which(is.nan(vectorToFix)))
   lastNaN <- max(which(is.nan(vectorToFix)))
   sizeToRemove <- ceiling(numSimulations*percentDataToRemove)
+  
+  #if there is sufficient data at tail end
+  if(firstNaN > sizeToRemove*1.5){
+  
   vectorToFix <- c( vectorToFix[1:(firstNaN - sizeToRemove)], rep(NaN, sizeToRemove), vectorToFix[(firstNaN + 1):length(vectorToFix)])
   vectorToFix <- zoo::na.approx(object = vectorToFix)
+
+  } else { #otherwise need to crudely approximate tail end
+    
+    #minimize distance between approximated slope and end point
+    
+    vectorToFix <- dataToFix[,"SimData"]
+    
+    slopeFractionSequence <- seq(from = 0,to = 0.1, by = 1/numSimulations)
+    
+    slopeDistances <- 
+    sapply(slopeFractionSequence
+           , function(x){
+             #x<-0
+             
+             slope <- vectorToFix[base::round(lastNaN+numSimulations*x + 1,0)] - vectorToFix[lastNaN + 1]
+             vectorToFix[firstNaN:lastNaN] <- NaN
+             for(xx in lastNaN:(ceiling(firstNaN+sizeToRemove/10))){vectorToFix[xx] <- vectorToFix[xx + 1] - slope}
+             #plot(vectorToFix])
+             
+             #get distance
+             NaNIDX <- which(is.nan(vectorToFix))
+             
+             distance <- vectorToFix[max(NaNIDX) + 1] - vectorToFix[min(NaNIDX) - 1]
+             return(distance)
+           }
+           )
+    
+    slopeFraction <- slopeFractionSequence[max(which(slopeDistances > 0))]
+    
+    slope <- vectorToFix[base::round(lastNaN+numSimulations*slopeFraction + 1,0)] - vectorToFix[lastNaN + 1]
+    vectorToFix[firstNaN:lastNaN] <- NaN
+    for(xx in lastNaN:(ceiling(firstNaN+sizeToRemove/10))){vectorToFix[xx] <- vectorToFix[xx + 1] - slope}
+    #plot(vectorToFix)
+    vectorToFix <- zoo::na.spline(vectorToFix)
+
+  }
   
   dataToFix <- cbind(dataToFix[,"Copula"], vectorToFix, dataToFix[,"OrigOrder"])
   colnames(dataToFix) <- dataToFixNames
