a3c0b3e2c1183fe309033226f9f8f747832ec4d6
erolbicero <erol.biceroglu@alumni.utoronto.ca>
2016-12-04 16:32:44 -0500

    function migrated to package tsc

--- R/99-Date-H-createMonthlySequence.R
+++
@@ -1,7 +0,0 @@
-createMonthlySequence <- function(start, end){
-  
-  resultSequence <- timeDate::timeSequence(from = start, to = end, by = "month")
-  resultSequence <- weekdayScrub(resultSequence)
-  return(resultSequence)
-  
-}
